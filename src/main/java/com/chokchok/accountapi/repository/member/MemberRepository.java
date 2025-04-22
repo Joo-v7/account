795ea1351e6f2c9c1e1efbbaf3e1e50dc08257cc
joo <helloworld.v7@gmail.com>
2025-04-22 21:52:02 +0900

    인증과 무관한 파일 삭제

--- src/main/java/com/chokchok/accountapi/repository/member/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chokchok.accountapi.repository.member;
-
-import com.chokchok.accountapi.domain.member.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- src/main/java/com/chokchok/accountapi/repository/role/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.chokchok.accountapi.repository.role;
-
-import com.chokchok.accountapi.domain.role.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
